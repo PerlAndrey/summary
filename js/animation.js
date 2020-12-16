cfbd761c642f026a7833b3ce5d43202b608d9ce8
Andrew <62856803+PerlAndrey@users.noreply.github.com>
2020-12-17 02:58:47 +0300

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -20,7 +20,7 @@ function loader(){
     
 
 
-function loader__frontend__verstka(){
+function loader__frontend(){
    document.getElementById("article__frontend__name").style.display = 'none'
    document.getElementById("article__frontend__verstka").style.display = 'none'
     document.getElementById("article__frontend__app").style.display = 'none'
